fce0b3caeaf7119982dbf2c11c4aee6927a604bf
JONATHAN P. BOND <jpbond@us.ibm.com>
2015-03-31 07:01:50 -0700

    First commit

--- js/composerBuilder.js
+++ js/composerBuilder.js
@@ -1,53 +1,12 @@
-var buildMatrixFromComposerJson = function(composerjson) {
-  var n = Object.keys(composerjson.require).length;
-  var matrix = [];
-  matrix[0] = [0];
-  var increment = 0;
-  // only the first element depends on others
-  for (var i=1; i < n; i++) {
-    matrix[0][i] = 1 + increment;
-    increment += 0.001;
-    matrix[i] = Array.apply(null, new Array(n)).map(Number.prototype.valueOf,0);
-    matrix[i][0] = 1;
-  }
-  var packageNames = Object.keys(composerjson.require);
-  packageNames.unshift(composerjson.name);
-  
-  return {
-    matrix: matrix,
-    packageNames: packageNames
-  }
-};
+var buildMatrixFromComposerJson = function(json) {
 
-var buildMatrixFromComposerJsonAndLock = function(composerjson, composerlock) {
-
-  var packages = composerlock.packages;
-  composerjson.isMain = true;
-  packages.unshift(composerjson);
+  var packages = json;
 
   var indexByName = {};
   var packageNames = {};
   var matrix = [];
   var n = 0;
   var replaces = {};
-
-  // List the replacements
-  packages.forEach(function(p) {
-    if (!p.replace) return;
-    for (replaced in p.replace) {
-      replaces[replaced] = p.name;
-    }
-  });
-  
-  // update required packages with replacements
-  packages.forEach(function(p) {
-    for (packageName in p.require) {
-      if (packageName in replaces) {
-        p.require[replaces[packageName]] = p.require[packageName];
-        delete p.require[packageName];
-      }
-    }
-  });
 
   // Compute a unique index for each package name.
   packages.forEach(function(p) {
